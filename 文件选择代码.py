264c0588f7cabb20b90d7d05302794c20358bae6
Lorence Cheng <lorence123@outlook.com>
2020-02-06 15:04:25 +0800

    这段代码通过选择目录，选择指定的文件，返回文件路径

--- 文件选择代码.py
+++ 文件选择代码.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 '''打开选择文件夹对话框'''
 root=tk.Tk()
-root.withdraw()
+root.withdraw() #撤除窗口
 Folderpath=filedialog.askdirectory() #获得选择好的文件夹
 Filepath=filedialog.askopenfilename() #获得选择好的文件
 print(Folderpath)
